b395bedbb3586ba3f603e34f051dc3bcc22e53f2
ilanzgx <ilan-_@hotmail.com>
2021-10-22 15:23:35 -0300

    Initial commit

---
+++ database/migrations/2021_10_22_165504_create_clientes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClientesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clientes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nome', 150);
+            $table->string('email', 100);
+            $table->string('senha', 200);
+            $table->string('cpf', 50);
+            $table->integer('telefone')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clientes');
+    }
+}
